c70e630ee55b03ddf71088f0e4c5befc59b57369
7 <swistek.labs@gmail.com>
2020-02-11 20:39:56 +0000

    generated controller and model

---
+++ db/migrate/20200211203627_create_user_auths.rb
@@ -0,0 +1,12 @@
+class CreateUserAuths < ActiveRecord::Migration
+  def change
+    create_table :user_auths do |t|
+      t.string] :[first_name[
+      t.string] :last_name[
+      t.string] :email[
+      t.string]] :password[
+
+      t.timestamps null: false
+    end
+  end
+end
