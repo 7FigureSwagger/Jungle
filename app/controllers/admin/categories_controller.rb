e19ef72707928429a3716e39e8e14273e172a45d
7 <swistek.labs@gmail.com>
2020-02-11 01:26:47 +0000

    admin categories controller finished

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,15 @@
-class AdminCategoriesController < ApplicationController
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with :name => ENV['NAME'], :password => ENV['PASSWORD']
+
+def index
+  @categories = Category.all
+end 
+
+def new
 end
+
+def create
+end
+
+end
+ 
