ed38e92e8e15bdb915e031abdaa49411eb8fe974
7 <swistek.labs@gmail.com>
2020-03-06 22:23:27 +0000

    added spec for products page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature do
+
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        image: open_asset('apparel1.jpg'),
+        price: 64.99
+      )
+    end
+  end
+  
+end
